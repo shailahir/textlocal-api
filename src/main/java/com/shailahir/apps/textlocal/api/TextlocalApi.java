153cd4d9077915311d27b02d79b4c8ed931988f7
Shailendra Ahir <shailendra@shailahir.com>
2020-07-25 11:19:53 +0530

    Issue #5 - Added Other methods to send message.

--- src/main/java/com/shailahir/apps/textlocal/api/TextlocalApi.java
+++ src/main/java/com/shailahir/apps/textlocal/api/TextlocalApi.java
@@ -8,4 +8,36 @@ import java.util.List;
 
 public interface TextlocalApi {
     public MessageSentResponse sendMessage(String message, List<String> numbers, String sender) throws TextlocalException, UnsupportedEncodingException;
+
+    public MessageSentResponse sendMessage(String message, List<String> numbers, String sender, String scheduleTime, String receiptUrl, String custom, boolean optoutsFlag, String validity, boolean unicodeFlag, boolean trackingLinksFlag) throws TextlocalException, UnsupportedEncodingException;
+
+    public MessageSentResponse sendMessage(String message, List<String> numbers, String sender, String scheduleTime, String receiptUrl, String custom, boolean optoutsFlag, String validity, boolean unicodeFlag) throws UnsupportedEncodingException, TextlocalException;
+
+    public MessageSentResponse sendMessage(String message, List<String> numbers, String sender, String scheduleTime, String receiptUrl, String custom, boolean optoutsFlag, String validity) throws UnsupportedEncodingException, TextlocalException;
+
+    public MessageSentResponse sendMessage(String message, List<String> numbers, String sender, String scheduleTime, String receiptUrl, String custom, boolean optoutsFlag) throws UnsupportedEncodingException, TextlocalException;
+
+    public MessageSentResponse sendMessage(String message, List<String> numbers, String sender, String scheduleTime, String receiptUrl, String custom) throws UnsupportedEncodingException, TextlocalException;
+
+    public MessageSentResponse sendMessage(String message, List<String> numbers, String sender, String scheduleTime, String receiptUrl) throws UnsupportedEncodingException, TextlocalException;
+
+    public MessageSentResponse sendMessage(String message, List<String> numbers, String sender, String scheduleTime) throws UnsupportedEncodingException, TextlocalException;
+
+    public MessageSentResponse sendMessage(String message, String groupId, String sender, String scheduleTime, String receiptUrl, String custom, boolean optoutsFlag, String validity, boolean unicodeFlag, boolean trackingLinksFlag) throws TextlocalException, UnsupportedEncodingException;
+
+    public MessageSentResponse sendMessage(String message, String groupId, String sender, String scheduleTime, String receiptUrl, String custom, boolean optoutsFlag, String validity, boolean unicodeFlag) throws TextlocalException, UnsupportedEncodingException;
+
+    public MessageSentResponse sendMessage(String message, String groupId, String sender, String scheduleTime, String receiptUrl, String custom, boolean optoutsFlag, String validity) throws TextlocalException, UnsupportedEncodingException;
+
+    public MessageSentResponse sendMessage(String message, String groupId, String sender, String scheduleTime, String receiptUrl, String custom, boolean optoutsFlag) throws TextlocalException, UnsupportedEncodingException;
+
+    public MessageSentResponse sendMessage(String message, String groupId, String sender, String scheduleTime, String receiptUrl, String custom) throws TextlocalException, UnsupportedEncodingException;
+
+    public MessageSentResponse sendMessage(String message, String groupId, String sender, String scheduleTime, String receiptUrl) throws TextlocalException, UnsupportedEncodingException;
+
+    public MessageSentResponse sendMessage(String message, String groupId, String sender, String scheduleTime) throws TextlocalException, UnsupportedEncodingException;
+
+    public MessageSentResponse sendMessage(String message, String groupId, String sender) throws TextlocalException, UnsupportedEncodingException;
+
+
 }
--- src/main/java/com/shailahir/apps/textlocal/api/impl/TextlocalApiImpl.java
+++ src/main/java/com/shailahir/apps/textlocal/api/impl/TextlocalApiImpl.java
@@ -10,6 +10,7 @@ import com.shailahir.apps.textlocal.utils.NetworkHelper;
 import com.shailahir.apps.textlocal.utils.Utils;
 
 import java.io.UnsupportedEncodingException;
+import java.net.URL;
 import java.net.URLEncoder;
 import java.util.List;
 
@@ -26,23 +27,156 @@ public class TextlocalApiImpl implements TextlocalApi {
         networkHelper = new NetworkHelper();
     }
 
-    public MessageSentResponse sendMessage(String message, List<String> numbers, String sender) throws TextlocalException, UnsupportedEncodingException {
+    public MessageSentResponse sendMessage(String message, List<String> numbers, String sender)
+            throws TextlocalException, UnsupportedEncodingException {
+        return sendMessage(message, numbers, sender, null);
+    }
+
+    public MessageSentResponse sendMessage(String message, List<String> numbers, String sender, String scheduleTime,
+                                           String receiptUrl, String custom, boolean optoutsFlag, String validity,
+                                           boolean unicodeFlag, boolean trackingLinksFlag)
+            throws TextlocalException, UnsupportedEncodingException {
+        StringBuffer buffer = prepareApiCall(message, numbers, sender, scheduleTime, receiptUrl, custom, optoutsFlag,
+                validity, unicodeFlag, trackingLinksFlag, null);
+        String jsonResponse = sendToApi(config.getSendMessageUrl(), buffer.toString());
+        return JsonHelper.extractMessageSentResponse(jsonResponse);
+    }
+
+    private StringBuffer prepareApiCall(String message, List<String> numbers, String sender, String scheduleTime,
+                                        String receiptUrl, String custom, boolean optoutsFlag, String validity, boolean unicodeFlag,
+                                        boolean trackingLinksFlag, String groupId)
+            throws TextlocalException, UnsupportedEncodingException {
         validateMessage(message);
         sender = validateSender(sender);
-        validateNumbers(numbers);
         StringBuffer buffer = new StringBuffer("apikey=");
         buffer.append(this.config.getApiKey());
         buffer.append("&message=");
         buffer.append(URLEncoder.encode(message, "UTF-8"));
         buffer.append("&sender=");
         buffer.append(sender);
-        buffer.append("&numbers=");
-        buffer.append(Utils.joinStrings(numbers));
+        if (numbers != null) {
+            validateNumbers(numbers);
+            buffer.append("&numbers=");
+            buffer.append(Utils.joinStrings(numbers));
+        } else {
+            buffer.append("&group_id=");
+            buffer.append(URLEncoder.encode(groupId, "UTF-8"));
+        }
+        if (scheduleTime != null) {
+            validateUnixTimestamp(scheduleTime);
+            buffer.append("&schedule_time=");
+            buffer.append(URLEncoder.encode(scheduleTime, "UTF-8"));
+        }
+        if (receiptUrl != null) {
+            validateUrl(receiptUrl);
+            buffer.append("&receipt_url=");
+            buffer.append(URLEncoder.encode(receiptUrl, "UTF-8"));
+        }
+        if (custom != null) {
+            buffer.append("&custom=");
+            buffer.append(URLEncoder.encode(custom, "UTF-8"));
+        }
+        if (optoutsFlag) {
+            buffer.append("&optouts=true");
+        }
+        if (validity != null) {
+            validateUnixTimestamp(validity);
+            buffer.append("&validity=");
+            buffer.append(URLEncoder.encode(validity, "UTF-8"));
+        }
+        if (unicodeFlag) {
+            buffer.append("&unicode=true");
+        }
+        if (trackingLinksFlag) {
+            buffer.append("&tracking_links=true");
+        }
         if (config.getTestMode()) {
             buffer.append("&test=true");
         }
+        return buffer;
+    }
+
+    public MessageSentResponse sendMessage(String message, List<String> numbers, String sender, String scheduleTime,
+                                           String receiptUrl, String custom, boolean optoutsFlag, String validity,
+                                           boolean unicodeFlag) throws UnsupportedEncodingException, TextlocalException {
+        return sendMessage(message, numbers, sender, scheduleTime, receiptUrl, custom, optoutsFlag, validity,
+                unicodeFlag, false);
+    }
+
+    public MessageSentResponse sendMessage(String message, List<String> numbers, String sender, String scheduleTime,
+                                           String receiptUrl, String custom, boolean optoutsFlag, String validity)
+            throws UnsupportedEncodingException, TextlocalException {
+        return sendMessage(message, numbers, sender, scheduleTime, receiptUrl, custom, optoutsFlag, validity, false);
+    }
+
+    public MessageSentResponse sendMessage(String message, List<String> numbers, String sender, String scheduleTime,
+                                           String receiptUrl, String custom, boolean optoutsFlag)
+            throws UnsupportedEncodingException, TextlocalException {
+        return sendMessage(message, numbers, sender, scheduleTime, receiptUrl, custom, optoutsFlag, null);
+    }
+
+    public MessageSentResponse sendMessage(String message, List<String> numbers, String sender, String scheduleTime,
+                                           String receiptUrl, String custom)
+            throws UnsupportedEncodingException, TextlocalException {
+        return sendMessage(message, numbers, sender, scheduleTime, receiptUrl, custom, false);
+    }
+
+    public MessageSentResponse sendMessage(String message, List<String> numbers, String sender, String scheduleTime,
+                                           String receiptUrl) throws UnsupportedEncodingException, TextlocalException {
+        return sendMessage(message, numbers, sender, scheduleTime, receiptUrl, null);
+    }
+
+    public MessageSentResponse sendMessage(String message, List<String> numbers, String sender, String scheduleTime)
+            throws UnsupportedEncodingException, TextlocalException {
+        return sendMessage(message, numbers, sender, scheduleTime, null);
+    }
+
+    public MessageSentResponse sendMessage(String message, String groupId, String sender, String scheduleTime,
+                                           String receiptUrl, String custom, boolean optoutsFlag, String validity,
+                                           boolean unicodeFlag, boolean trackingLinksFlag)
+            throws TextlocalException, UnsupportedEncodingException {
+        StringBuffer buffer = prepareApiCall(message, null, sender, scheduleTime, receiptUrl, custom, optoutsFlag,
+                validity, unicodeFlag, trackingLinksFlag, groupId);
         String jsonResponse = sendToApi(config.getSendMessageUrl(), buffer.toString());
         return JsonHelper.extractMessageSentResponse(jsonResponse);
+    }
+
+    public MessageSentResponse sendMessage(String message, String groupId, String sender, String scheduleTime, String receiptUrl,
+                                           String custom, boolean optoutsFlag, String validity, boolean unicodeFlag)
+            throws TextlocalException, UnsupportedEncodingException {
+        return sendMessage(message, groupId, sender, scheduleTime, receiptUrl, custom, optoutsFlag, validity, unicodeFlag, false);
+    }
+
+    public MessageSentResponse sendMessage(String message, String groupId, String sender, String scheduleTime, String receiptUrl,
+                                           String custom, boolean optoutsFlag, String validity)
+            throws TextlocalException, UnsupportedEncodingException {
+        return sendMessage(message, groupId, sender, scheduleTime, receiptUrl, custom, optoutsFlag, validity, false);
+    }
+
+    public MessageSentResponse sendMessage(String message, String groupId, String sender, String scheduleTime, String receiptUrl,
+                                           String custom, boolean optoutsFlag)
+            throws TextlocalException, UnsupportedEncodingException {
+        return sendMessage(message, groupId, sender, scheduleTime, receiptUrl, custom, optoutsFlag, null);
+    }
+
+    public MessageSentResponse sendMessage(String message, String groupId, String sender, String scheduleTime, String receiptUrl,
+                                           String custom) throws TextlocalException, UnsupportedEncodingException {
+        return sendMessage(message, groupId, sender, scheduleTime, receiptUrl, custom, false);
+    }
+
+    public MessageSentResponse sendMessage(String message, String groupId, String sender, String scheduleTime, String receiptUrl)
+            throws TextlocalException, UnsupportedEncodingException {
+        return sendMessage(message, groupId, sender, scheduleTime, receiptUrl, null);
+    }
+
+    public MessageSentResponse sendMessage(String message, String groupId, String sender, String scheduleTime)
+            throws TextlocalException, UnsupportedEncodingException {
+        return sendMessage(message, groupId, sender, scheduleTime, null);
+    }
+
+    public MessageSentResponse sendMessage(String message, String groupId, String sender)
+            throws TextlocalException, UnsupportedEncodingException {
+        return sendMessage(message, groupId, sender, null);
     }
 
     private String sendToApi(String url, String data) {
@@ -52,8 +186,21 @@ public class TextlocalApiImpl implements TextlocalApi {
         return networkHelper.post(url, data);
     }
 
+    private void validateUrl(String receiptUrl) throws TextlocalException {
+        try {
+            URL url = new URL(receiptUrl);
+            url.toURI();
+        } catch (Exception e) {
+            throw new TextlocalException(-1, "Invalid URL");
+        }
+    }
+
+    private void validateUnixTimestamp(String scheduleTime) {
+        // TODO - Find and validate the proper format
+    }
+
     private void validateNumbers(List<String> numbers) throws TextlocalException {
-        if (numbers == null || numbers.size() == 0) {
+        if (numbers.size() == 0) {
             throw new TextlocalException(TextlocalConstants.Errors.NO_RECIPIENTS);
         }
         if (numbers.size() > 10000) {
